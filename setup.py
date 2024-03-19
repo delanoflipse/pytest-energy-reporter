8b5e680d75783273c3a6ca883fd85fe3ee69d042
Delano Flipse <delanoflipse@hotmail.com>
2024-03-19 13:00:02 +0100

    chore: placeholder code

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name="pytest-energy-reporter",
+    version='0.1.0',
+    description='A energy estimation reporter for pytest',
+    url='https://github.com/delanoflipse/pytest-energy-reporter',
     packages=["pytest-energy-reporter"],
     # the following makes a plugin available to pytest
     entry_points={"pytest11": ["pytest_energy_reporter = pytest-energy-reporter.plugin"]},
