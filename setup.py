4b6b7f70b82e7a31044b344b25cf9897803886ad
Delano Flipse <delanoflipse@hotmail.com>
2024-03-27 08:53:09 +0100

    chore: converge versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pytest_energy_reporter",
-    version='0.1.0',
+    version='0.2.0',
     description='A energy estimation reporter for pytest',
     url='https://github.com/delanoflipse/pytest-energy-reporter',
     packages=["pytest_energy_reporter"],
