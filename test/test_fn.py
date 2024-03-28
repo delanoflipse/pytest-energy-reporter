33230ea67f6d34a69d8207b2fc2e97b6b585c187
Delano Flipse <delanoflipse@hotmail.com>
2024-03-28 21:04:09 +0100

    chore: uncomment commented tests...

--- test/test_fn.py
+++ test/test_fn.py
@@ -26,57 +26,57 @@ def test_fib_35_n3():
 def test_fib_35_n5():
   fib(35)
 
-# @pytest.mark.energy(n=10)
-# def test_fib_35_n10():
-#   fib(35)
+@pytest.mark.energy(n=10)
+def test_fib_35_n10():
+  fib(35)
 
-# @pytest.mark.energy(n=20)
-# def test_fib_35_n20():
-#   fib(35)
+@pytest.mark.energy(n=20)
+def test_fib_35_n20():
+  fib(35)
 
-# @pytest.mark.energy
-# def test_fib_35():
-#   fib(35)
+@pytest.mark.energy
+def test_fib_35():
+  fib(35)
 
-# @pytest.mark.energy
-# def test_fib_slow_35():
-#   sleep(2)
-#   fib(35)
+@pytest.mark.energy
+def test_fib_slow_35():
+  sleep(2)
+  fib(35)
 
-# @pytest.mark.energy
-# def test_fib_35_2x():
-#   fib(35)
-#   fib(35)
+@pytest.mark.energy
+def test_fib_35_2x():
+  fib(35)
+  fib(35)
 
-# @pytest.mark.energy
-# def test_fib_34():
-#   fib(34)
+@pytest.mark.energy
+def test_fib_34():
+  fib(34)
 
-# @pytest.mark.energy
-# def test_fib_34_2x():
-#   fib(34)
-#   fib(34)
+@pytest.mark.energy
+def test_fib_34_2x():
+  fib(34)
+  fib(34)
 
-# @pytest.mark.energy
-# def test_fib_34_3x():
-#   fib(34)
-#   fib(34)
-#   fib(34)
+@pytest.mark.energy
+def test_fib_34_3x():
+  fib(34)
+  fib(34)
+  fib(34)
 
-# @pytest.mark.energy
-# def test_sleep_2s():
-#   sleep(2)
+@pytest.mark.energy
+def test_sleep_2s():
+  sleep(2)
 
-# @pytest.mark.energy
-# def test_sleep_5s():
-#   sleep(5)
+@pytest.mark.energy
+def test_sleep_5s():
+  sleep(5)
 
-# def test_fib_assert_w():
-#   energy, res, err = measure(lambda: fib(34), n=3)
-#   assert res == 5702887
-#   assert err == None
-#   assert energy.power_w < 200
+def test_fib_assert_w():
+  energy, res, err = measure(lambda: fib(34), n=3)
+  assert res == 5702887
+  assert err == None
+  assert energy.power_w < 200
 
-# def test_fib_assert_j():
-#   energy = measure_energy(lambda: fib(34), n=2)
-#   assert energy.energy_j < 1000
+def test_fib_assert_j():
+  energy = measure_energy(lambda: fib(34), n=2)
+  assert energy.energy_j < 1000
